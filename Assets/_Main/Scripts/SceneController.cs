da68b441fd84235c415090b65fd2556055fd1ce8
januarelsan <januarelsan@gmail.com>
2022-01-26 14:17:16 +0700

    Add Scene Controller Script

---
+++ Assets/_Main/Scripts/SceneController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneController : Singleton<SceneController> {
+
+	
+	public void RestartScene (){
+		SceneManager.LoadScene (SceneManager.GetActiveScene().name);
+	}
+
+	public void GoToScene(string sceneName){		
+		SceneManager.LoadScene (sceneName);
+
+	}
+
+	public void QuitGame(){		
+		Application.Quit ();
+	}
+	
+		
+}
